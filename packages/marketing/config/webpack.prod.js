0bda3b6e98ea6679c88bdffc3262f1abb6bf2bdc
DawitFsiha <dawit@terabit.ca>
2022-04-11 18:51:59 -0400

    created container deploy script

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,15 +1,25 @@
-module.exports = {
-  module:{
-    rules:{
-      test:/\.m?js$/,
-      exclude: /node_modules/, 
-      use: {
-        loader: 'babel-loader',
-        options: {
-          preset: ['@babel/preset-react', '@babel/preset-env'],
-          plugins: ['@babel/plugin-transform-runtime'],
-        },
-      }
-    }
-  }
-}
+const {merge} = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson= require('../package.json');
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: 'production',
+  output:{
+    filename: '[name].[contenthash].js'
+  },
+  plugins:[
+    new ModuleFederationPlugin({
+      name: 'container',
+      filename:'remoteEntry.js',
+      exposes:{
+            './MarketingApp':'./src/bootstrap'
+      },
+      shared: packageJson.dependencies
+    })
+  ]
+  
+};
+module.exports= merge(commonConfig, prodConfig);
